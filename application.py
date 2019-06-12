bcc074e2cf78dd55da80a43e902f747f97de9a33
SofiDicosta81 <51751870+SofiDicosta81@users.noreply.github.com>
2019-06-12 21:38:24 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+#, jsonify, request
 
 app = Flask(__name__)
 
@@ -10,7 +11,8 @@ def index():
         #request.GET_json()
     #    return jsonify({'You send': some_json}), 201
     #else:
-    return jsonify({'about': 'Hello world'})
+    #return jsonify({'about': 'Hello world'})
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(debug=True)
