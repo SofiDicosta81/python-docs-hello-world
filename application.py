1eb8aa3946c5a04810e71654559e0e8175cb9673
SofiDicosta81 <51751870+SofiDicosta81@users.noreply.github.com>
2019-06-12 21:34:06 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if (request.method == 'POST'):
-        some_json = "Sunesh" 
+    #if (request.method == 'POST'):
+    #    some_json = "Sunesh" 
         #request.GET_json()
-        return jsonify({'You send': some_json}), 201
-    else:
-        return jsonify({'about': 'Hello world'}),
+    #    return jsonify({'You send': some_json}), 201
+    #else:
+    return jsonify({'about': 'Hello world'}),
 
 if __name__ == "__main__":
     app.run(debug=True)
