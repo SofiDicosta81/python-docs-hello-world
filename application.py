c178b8824866bda34c7a2d02e838d7451b219d55
SofiDicosta81 <51751870+SofiDicosta81@users.noreply.github.com>
2019-06-12 22:54:02 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,12 +5,15 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if (request.method == 'POST'):
-        some_json = request.GET_json()
-        return jsonify({'You send': some_json}), 201
-    else:
-    return jsonify({'about': 'Hello world'})
-    #return "Hello World!"
+    return jsonify({'about': 'Hello world'}),
+
+@app.route('/multi/<int:num>', methods=['GET'])
+def get_multiply10(num):
+    return jsonify({'result' : num*10})
+
+@app.route('/add/<int:val>', methods=['GET'])
+def get_Sum(val):
+    return jsonify({'result': val+20})
 
 if __name__ == "__main__":
     app.run(debug=True)
